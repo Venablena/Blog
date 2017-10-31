d0227ec1d2d67d7b774e5be0cb279a8a9aa8aef4
Lena Venable <venablena@gmail.com>
2017-10-31 11:37:37 -0700

    restructure models for getAll

--- src/models/models.js
+++ src/models/models.js
@@ -5,10 +5,10 @@ const fs = require('fs')
 const filePath = path.join(__dirname, '../assets/db_posts.json')
 
 function getAll() {
-  console.log("Get all function here")
-  return JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-
-  //return JSON.stringify(result)
+  // console.log("Get all function here")
+  return JSON.stringify(fs.readFileSync(filePath, 'utf-8'))
+  // console.log(result);
+  // return result
 }
 
 module.exports = {
