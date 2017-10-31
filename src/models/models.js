9f106259adc12f08e88bc0d0fc2326019b5eec6b
Lena Venable <venablena@gmail.com>
2017-10-31 15:22:43 -0700

    getAll route

--- src/models/models.js
+++ src/models/models.js
@@ -5,12 +5,26 @@ const fs = require('fs')
 const filePath = path.join(__dirname, '../assets/db_posts.json')
 
 function getAll() {
+  const data = readDB()
+  data.forEach(item => {
+    console.log(item.title);
+    console.log(item.body);
+  })
+  // console.log(data.title);
+  // console.log(data.body);
+  //const allPosts = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
   // console.log("Get all function here")
-  return JSON.stringify(fs.readFileSync(filePath, 'utf-8'))
+  //return JSON.stringify(allPosts)
+  //return JSON.stringify(fs.readFileSync(filePath, 'utf-8'))
   // console.log(result);
-  // return result
+  //return data
 }
 
 module.exports = {
   getAll
+}
+
+function readDB(){
+  const post = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+  return post.data
 }
