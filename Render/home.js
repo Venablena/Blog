a18e596dabae43ce28371a01255123f5f5f25615
Lena Venable <venablena@gmail.com>
2017-11-07 13:22:59 -0800

    add post and get all working

--- Render/home.js
+++ Render/home.js
@@ -1,17 +1,5 @@
 console.log("This is home.js");
 
-// window.home = {
-//   init () {
-//     Call.getAll()
-//       .then(result => {
-//         document.querySelector('#container').innerHTML = Post.generateSnippets(result.data)
-//       })
-//       .catch(error => (error))
-//   }
-// }
-//
-// home.init()
-
 window.Render = {
   init() {
     Request.getAll()
--- Templates/index.js
+++ Templates/index.js
@@ -1,8 +1,9 @@
 console.log("hi, I'm index.js!");
 window.Template = {
   generateSnippets(data){
-    let array = data.map(item => {
-      const snippet = item.body.slice(0, 255) + '...'
+    const array = data.map(item => {
+      console.log(item);
+      const snippet = item.content.slice(0, 255) + '...'
       return `
         <div class="post-title">${item.title}</div>
         <div class="post-body">${snippet}</div>
