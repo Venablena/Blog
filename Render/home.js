6ce4e6a290e271ab201fc54f288433448f02241f
Lena Venable <venablena@gmail.com>
2017-11-07 12:22:27 -0800

    changed form formatting, everything happens on post page now

--- Render/home.js
+++ Render/home.js
@@ -1,13 +1,40 @@
-liveconsole.log("This is home.js");
+console.log("This is home.js");
 
-window.home = {
-  init () {
-    Call.getAll()
+// window.home = {
+//   init () {
+//     Call.getAll()
+//       .then(result => {
+//         document.querySelector('#container').innerHTML = Post.generateSnippets(result.data)
+//       })
+//       .catch(error => (error))
+//   }
+// }
+//
+// home.init()
+
+window.Render = {
+  init() {
+    Request.getAll()
       .then(result => {
-        document.querySelector('#container').innerHTML = Post.generateSnippets(result.data)
+        document.querySelector('#container').innerHTML = Template.generateSnippets(result.data)
       })
       .catch(error => (error))
+  },
+  newPost() {
+    Request.new()
+      // .then(result => {
+      //   console.log(result.data);
+      // })
   }
 }
-
-home.init()
+//Get all
+Render.init()
+//Create new
+document.querySelector('#createNew').addEventListener('submit', event => {
+  event.preventDefault()
+  const title = document.querySelector('#post-title').value
+  const content = document.querySelector('#post-body').value
+  //console.log({title, content});
+  Request.new({ title, content })
+  Render.init()
+})
