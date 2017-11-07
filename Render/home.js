49a4ace668dced81477d32e1e7fac76a6c56079e
Lena Venable <venablena@gmail.com>
2017-11-07 11:58:14 -0800

    remove old code

--- Render/home.js
+++ Render/home.js
@@ -1,27 +1,13 @@
-console.log("This is home.js");
+liveconsole.log("This is home.js");
 
-window.Render = {
-  init() {
-    Request.getAll()
+window.home = {
+  init () {
+    Call.getAll()
       .then(result => {
-        document.querySelector('#container').innerHTML = Template.generateSnippets(result.data)
+        document.querySelector('#container').innerHTML = Post.generateSnippets(result.data)
       })
       .catch(error => (error))
-  },
-  newPost() {
-    Request.new()
-      .then(result => {
-        console.log(result.data);
-      })
   }
 }
-//Get all
-Render.init()
-//Create new
-document.querySelector('#createNew').addEventListener('submit', event => {
-  event.preventDefault()
-  const title = document.querySelector('#post-title').value
-  const content = document.querySelector('#post-body').value
-  Request.new({ title, content })
-  Render.init()
-})
+
+home.init()
