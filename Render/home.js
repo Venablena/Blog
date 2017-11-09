094ed3878427194758d35dbba08e525cabaa9d06
Lena Venable <venablena@gmail.com>
2017-11-09 08:11:16 -0800

    event handlers sorted

--- Render/home.js
+++ Render/home.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', ()=> renderHome())
+
 function renderHome(){
   Request.getAll()
     .then((result) => {
@@ -10,19 +12,31 @@ function renderHome(){
     .catch(error => (error))
 }
 
-document.addEventListener('DOMContentLoaded', ()=> renderHome())
-// function editEvents(button){
-//   button.forEach(el => {
-//     el.addEventListener('click', () => {
-//       const id = el.previousSibling.previousSibling.textContent
-//       Request.show(id)
-//         .then((result) => {
-//           document.querySelector('#container').innerHTML = templateForm(
-//             post = { id:result.data.id, title: result.data.title, content: result.data.content }, 'updateForm')
-//         })
-//     })
-//   })
-// }
+function editEvents(button){
+  button.forEach(el => {
+    el.addEventListener('click', () => {
+      const id = el.previousSibling.previousSibling.textContent
+      Request.show(id)
+        .then((result) => {
+          document.querySelector('#container').innerHTML = templateForm(
+            post = { id:result.data.id, title: result.data.title, content: result.data.content }, 'updateForm')
+            updateEvent(document.querySelector('#updateForm'))
+         document.querySelector('#cancel').addEventListener('click', renderHome)
+        })
+    })
+  })
+}
+
+function updateEvent(form){
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    const title = document.querySelector('#post-title').value
+    const content = document.querySelector('#post-body').value
+    return Request.update({ title, content })
+      .then(() => renderHome())
+      .catch(error => (error))
+  })
+}
 
 function deleteEvents(button){
   button.forEach(el => {
@@ -42,6 +56,9 @@ function readMore(button){
        Request.show(id)
        .then((result) => {
          document.querySelector('#container').innerHTML = showOne(result.data)
+         document.querySelector('#cancel').addEventListener('click', renderHome)
+         editEvents(document.querySelectorAll('.btn-edit'))
+
        }).catch(error => (error))
     })
   })
