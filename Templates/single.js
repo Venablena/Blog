335d935d5311a633aa539e682f3d5a93e24d1086
Lena Venable <venablena@gmail.com>
2017-11-27 17:29:18 -0800

    styling

--- Templates/single.js
+++ Templates/single.js
@@ -24,15 +24,12 @@ function randomStyle(array){
 const fonts = [
   "Gloria Hallelujah",
   "Amatic SC",
-  "Permanent Marker",
   "Shadows Into Light",
   "Shadows Into Light Two",
-  "Sacramento",
   "Homemade Apple",
   "Covered By Your Grace",
   "Reenie Beanie",
   "Sue Ellen Francisco",
-  "Dawning of a New Day",
   "Waiting for the Sunrise",
   "Loved by the King",
   "Crafty Girls",
@@ -40,17 +37,14 @@ const fonts = [
 ]
 
 const papers = [
-  "_original.jpeg",
   "3a340f781fa474279eddc60a00e98bc5c174c62d.jpeg",
   "2144.jpg",
   "2270.jpg",
   "3201.jpg",
   "3718.jpg",
-  "6320.jpg",
   "6530.jpg",
   "abstract-2055560_960_720.jpg",
   "background-paper-797285_960_720.jpg",
   "AuroraWienhold-DeviantArt-47629.jpg",
-  "watercolor-2418958_960_720.jpg",
   "watercolour_texture_by_summer__moon-1.jpg"
 ]
